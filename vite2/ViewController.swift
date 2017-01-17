9f6d5503c3bc1deffcc288e3165b3ea79037899f
Steven Grutman <stevengrutman@stevens-mbp.home>
2017-01-17 15:17:38 -0500

    Initial Commit

---
+++ vite2/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  vite2
+//
+//  Created by Steven Grutman on 1/17/17.
+//  Copyright © 2017 Steven Grutman. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
